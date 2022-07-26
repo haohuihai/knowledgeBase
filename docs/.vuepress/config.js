218de84b216b48ed2ec9afd4f2887cb5111acc0b
haohuihai <157855644@qq.com>
2022-07-26 22:10:38 +0800

    fix: 整理目录

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -4,62 +4,126 @@ module.exports = {
   dest: 'docs/dist', /* 打包文件基础路径, 在命令所在目录下 */
   title: '技术小记', // 标题
   description: '记录一些技术和生活', // 标题下的描述
-  
+
   themeConfig: { // 主题配置
-    logo: '/logo.png',
-    sidebarDepth: 2,
+    logo: '/images/logo.jpeg',
+    sidebarDepth: 0,
     nav: [ // 头部导航
-      { text: '官网', link: 'http://www.atguigu.com' },
+      { text: '官网', link: 'http://101.132.124.188' },
       { text: '谷粒学院', link: 'http://www.gulixueyuan.com/' },
-      { 
-        text: '学习路线', 
+      {
+        text: '学习路线',
         items: [
           { text: '前端', link: 'http://www.atguigu.com/web/' },
           { text: 'Java', link: 'http://www.atguigu.com/kecheng.shtml' },
           { text: '大数据', link: 'http://www.atguigu.com/bigdata/' }
-        ] 
+        ]
       },
     ],
     sidebar: [ // 左侧导航
       {
         title: '技术方面', // 标题
-        collapsable: false, // 下级列表不可折叠
+        collapsable: true, // 下级列表不可折叠
         children: [ // 下级列表
-          { 
+          {
             title: '前端杂记',
             collapsable: true,
             children: [
-
               {
-                title: '初级',
+                title: 'CSS',
                 collapsable: true,
                 children: [
                   {
-                    title: 'CSS',
-                    collapsable: true,
-                    children: [
-                      {
-                        title: 'CSS1',
-                        path: '../pages/前端/CSS/CSS1',
-                      },
-                      {
-                        title: 'CSS2',
-                        path: '../pages/前端/CSS/CSS2',
-                      }
-                    ]
+                    title: 'CSS1',
+                    path: '../pages/前端/CSS/CSS1',
+                  },
+                  {
+                    title: 'CSS2',
+                    path: '../pages/前端/CSS/CSS2',
                   }
-                  
                 ]
-              }
+              },
+              {
+                title: 'JavaScript',
+                collapsable: true,
+                children: [
+                  {
+                    title: '阶段一',
+                    path: '../pages/前端/JS/阶段一',
+                  },
+                  {
+                    title: '阶段二',
+                    path: '../pages/前端/JS/阶段二',
+                  },
+                  {
+                    title: '阶段三',
+                    path: '../pages/前端/JS/阶段三',
+                  },
+                  {
+                    title: '阶段四',
+                    path: '../pages/前端/JS/阶段四',
+                  }
+                ]
+              },
             ]
           },
           { title: 'Node相关', path: '../pages/Node/aaa' },
           { title: 'Python', path: '../pages/Python/python基础' },
         ]
-      }
+      },
+      {
+        title: '部署', // 标题
+        collapsable: true, // 下级列表不可折叠
+        children: [ // 下级列表
+          {
+            title: '前端杂记',
+            collapsable: true,
+            children: [
+              {
+                title: 'CSS',
+                collapsable: true,
+                children: [
+                  {
+                    title: 'CSS1',
+                    path: '../pages/前端/CSS/CSS1',
+                  },
+                  {
+                    title: 'CSS2',
+                    path: '../pages/前端/CSS/CSS2',
+                  }
+                ]
+              },
+              {
+                title: 'JavaScript',
+                collapsable: true,
+                children: [
+                  {
+                    title: '阶段一',
+                    path: '../pages/前端/JS/阶段一',
+                  },
+                  {
+                    title: '阶段二',
+                    path: '../pages/前端/JS/阶段二',
+                  },
+                  {
+                    title: '阶段三',
+                    path: '../pages/前端/JS/阶段三',
+                  },
+                  {
+                    title: '阶段四',
+                    path: '../pages/前端/JS/阶段四',
+                  }
+                ]
+              },
+            ]
+          },
+          { title: 'Node相关', path: '../pages/Node/aaa' },
+          { title: 'Python', path: '../pages/Python/python基础' },
+        ]
+      },
     ],
-  head: [ // 指定网页head图标
-    ['link', { rel: 'shortcut icon', type: "image/x-icon", href: `/favicon.ico` }]
-  ]
-}
+    head: [ // 指定网页head图标
+      ['link', { rel: 'shortcut icon', type: "image/x-icon", href: `/favicon.ico` }]
+    ]
+  }
 }
